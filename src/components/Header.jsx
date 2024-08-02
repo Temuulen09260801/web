839d526820f6959c1b34e878d115fdedbb249ddd
GereltuyaMz <gegiimz96@gmail.com>
2024-08-02 11:45:36 +0800

    add pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+	return (
+		<header className="flex max-w-[1200px] m-auto justify-between p-5 items-center">
+			<img src="./images/metaLogo.png" alt="logo" className="w-36 h-8" />
+			<ul className="flex gap-10">
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/blog">Blog</Link>
+				</li>
+				<li>
+					<Link href="/contact">Contact</Link>
+				</li>
+			</ul>
+			<input
+				type="text"
+				placeholder="Search"
+				className="border border-solid p-1"
+			/>
+		</header>
+	);
+};
+
+export default Header;
